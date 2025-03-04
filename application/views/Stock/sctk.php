1aff4b654b2603ac29f54be0db38e46ccbc778b2
chllanes <milciade.anals@msn.com>
2025-03-04 08:48:53 -0300

    Eliminación de archivos del repositorio

--- application/views/Stock/sctk.php
+++
@@ -1,9 +0,0 @@
-    <script  type="text/javascript" charset="utf-8" async defer>
-       var data = <?php echo $this->session->userdata('urisegment');?>;
-       $(function() {
-			if (data == 0 || data > 0) {
-				stocktodas(data);
-				<?php $this->session->set_userdata('urisegment',null);?>
-			}
-       });
-    </script>
